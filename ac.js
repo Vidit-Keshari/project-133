7bfd44d8ac894f05c82220171247c462358973ae
Vidit Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2023-10-13 19:22:49 +0530

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,50 @@
+status = "";
+img = "";
+object = [];
+
+function preload() {
+    img = loadImage("ac.png");
+}
+
+function setup() {
+    canvas = createCanvas(600, 430);
+    canvas.position(375, 270);
+    objectDetector = ml5.objectDetector('cococssd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: detecting objects";
+}
+
+function draw() {
+    image(img, 0, 0, 600, 430);
+    if(status != "") {
+        for(i = 0; i < object.length; i++) {
+            confidence = object[i].confidence;
+            percent = floor(confidence * 100);
+            label = object[i].label;
+            x = objecs[i].x;
+            y = object[i].y;
+            width = object[i].width;
+            height = object[i].height;
+            fill('#FF0000');
+            text(label, x, y);
+            noFill();
+            stroke('#FF0000');
+            rect(x, y, width, height);
+            noStroke()
+        }
+    }
+    
+}
+
+function modelLoaded() {
+    console.log("model Loaded Successfully");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error();
+    }
+    console.log(results);
+    object = results;
+}
---
+++ main.js
@@ -0,0 +1,23 @@
+function ac() {
+    window.location = "ac.html";
+}
+
+function home() {
+    window.location = "index.html";
+}
+
+function bedroom() {
+    window.location = "bedroom.html";
+}
+
+function desk() {
+    window.location = "desk.html";
+}
+
+function tv() {
+    window.location = "tv.html";
+}
+
+function drawing_room() {
+    window.location = "drawing_room.html";
+}
